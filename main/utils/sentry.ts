91a69f8f18c31829dfedd90ceb4f553cdbfe1e0b
qiyang.mqy <zjmqydes@163.com>
2023-12-21 14:33:42 +0800

    refactor: adjust sentry replay

--- main/utils/sentry.ts
+++ main/utils/sentry.ts
@@ -1,19 +1,36 @@
-'use strict';
+"use strict";
 
-import {app} from 'electron';
-import {is} from 'electron-util';
-import * as Sentry from '@sentry/electron';
-import {settings} from '../common/settings';
+import { app } from "electron";
+import { is } from "electron-util";
+import * as Sentry from "@sentry/electron/renderer";
+import { settings } from "../common/settings";
 
-const SENTRY_PUBLIC_DSN = 'https://04c3d4bad20820a0f358028f28e04d16@o4505409681948672.ingest.sentry.io/4506432195461120';
+const SENTRY_PUBLIC_DSN =
+  "https://04c3d4bad20820a0f358028f28e04d16@o4505409681948672.ingest.sentry.io/4506432195461120";
 
-export const isSentryEnabled = !is.development && settings.get('allowAnalytics');
+export const isSentryEnabled =
+  !is.development && settings.get("allowAnalytics");
 
 if (isSentryEnabled) {
   const release = `${app.name}@${app.getVersion()}`.toLowerCase();
   Sentry.init({
     dsn: SENTRY_PUBLIC_DSN,
-    release
+    release,
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
+
+    // If the entire session is not sampled, use the below sample rate to sample
+    // sessions when an error occurs.
+    replaysOnErrorSampleRate: 1.0,
+
+    integrations: [
+      new Sentry.Replay({
+        // Additional SDK configuration goes in here, for example:
+        maskAllText: true,
+        blockAllMedia: true,
+      }),
+    ],
   });
 }
 
--- renderer/utils/sentry-error-boundary.tsx
+++ renderer/utils/sentry-error-boundary.tsx
@@ -1,29 +1,49 @@
-import React from 'react';
-import * as Sentry from '@sentry/browser';
-import electron from 'electron';
-import type {api as Api, is as Is} from 'electron-util';
+import React from "react";
+import * as Sentry from "@sentry/browser";
+import electron from "electron";
+import type { api as Api, is as Is } from "electron-util";
 
-const SENTRY_PUBLIC_DSN = 'https://04c3d4bad20820a0f358028f28e04d16@o4505409681948672.ingest.sentry.io/4506432195461120';
+const SENTRY_PUBLIC_DSN =
+  "https://04c3d4bad20820a0f358028f28e04d16@o4505409681948672.ingest.sentry.io/4506432195461120";
 
-class SentryErrorBoundary extends React.Component<{children: React.ReactNode}> {
+class SentryErrorBoundary extends React.Component<{
+  children: React.ReactNode;
+}> {
   constructor(props) {
     super(props);
-    const {settings} = electron.remote.require('./common/settings');
+    const { settings } = electron.remote.require("./common/settings");
     // Done in-line because this is used in _app
-    const {is, api} = require('electron-util') as {
+    const { is, api } = require("electron-util") as {
       api: typeof Api;
       is: typeof Is;
     };
 
-    if (!is.development && settings.get('allowAnalytics')) {
+    if (!is.development && settings.get("allowAnalytics")) {
       const release = `${api.app.name}@${api.app.getVersion()}`.toLowerCase();
-      Sentry.init({dsn: SENTRY_PUBLIC_DSN, release});
+      Sentry.init({
+        dsn: SENTRY_PUBLIC_DSN,
+        release, // This sets the sample rate to be 10%. You may want this to be 100% while
+        // in development and sample at a lower rate in production
+        replaysSessionSampleRate: 0.1,
+
+        // If the entire session is not sampled, use the below sample rate to sample
+        // sessions when an error occurs.
+        replaysOnErrorSampleRate: 1.0,
+
+        integrations: [
+          new Sentry.Replay({
+            // Additional SDK configuration goes in here, for example:
+            maskAllText: true,
+            blockAllMedia: true,
+          }),
+        ],
+      });
     }
   }
 
   componentDidCatch(error, errorInfo) {
     console.log(error, errorInfo);
-    Sentry.configureScope(scope => {
+    Sentry.configureScope((scope) => {
       for (const [key, value] of Object.entries(errorInfo)) {
         scope.setExtra(key, value);
       }
